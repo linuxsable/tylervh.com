27aea6840a40cce14e7f1e77701b21c7692df6e4
Tyler Van Hoomissen <tvanh@atlassian.com>
2020-04-18 21:34:57 -0700

    Add redirects

--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -11,24 +11,29 @@ const getFriendlyDate = (dateJSON: string) => {
 };
 
 export const IndexPage: React.FC = () => {
+  const atlassianLink = (
+    <a
+      href="http://atlassian.com"
+      className="black-70 link hover-black underline"
+    >
+      Atlassian
+    </a>
+  );
+
+  const atomStatesLink = (
+    <a
+      href="http://atomstates.bandcamp.com"
+      className="black-70 link hover-black underline"
+    >
+      Atom States
+    </a>
+  );
+
   return (
     <div>
       <p className="measure-wide lh-copy">
-        Tyler builds software for teams at
-        <a
-          href="http://atlassian.com"
-          className="black-70 link hover-black underline"
-        >
-          Atlassian
-        </a>
-        and creates modular analogue music under the moniker
-        <a
-          href="http://atomstates.bandcamp.com"
-          className="black-70 link hover-black underline"
-        >
-          Atom States
-        </a>
-        .
+        Tyler builds software for teams at {atlassianLink} and creates modular
+        analogue music under the moniker {atomStatesLink}.
       </p>
 
       <ul className="list f5 pl0 mt4 mb0">
